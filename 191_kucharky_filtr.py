ddd4acf137e0ad0f045de1c1639b75874c2d41db
Michal Kašpárek <2658013+michalkasparek@users.noreply.github.com>
2026-09-16 15:32:27 +0200

    pečlivější filtr kuchařek

--- 191_kucharky_filtr.py
+++ 191_kucharky_filtr.py
@@ -60,21 +60,25 @@ def _(pl):
 @app.cell
 def _(df):
     df
+    return
 
 
 @app.cell
 def _(df, pl):
     df.filter(pl.col("100_a").str.contains("Hejlík, Lu"))
+    return
 
 
 @app.cell
 def _(df, pl):
     df.filter(pl.col("245_a").str.contains("(?i)gastro"))
+    return
 
 
 @app.cell
 def _(df, pl):
     df.filter(pl.col("245_a").str.contains("(?i)kluci v akci"))
+    return
 
 
 @app.cell
@@ -84,53 +88,56 @@ def _(lf, pl):
     sloupec_keyword_pozitivni = [
         ("072_x", "Kuchařství. Potraviny. Vařená jídla"),
         ("246_a", "kuchařka"),
-        ("650_a", "jídla"),
-        ("650_a", "meals"),
-        ("650_a", "kuchařství"),
-        ("655_a", "kuchařské recepty"),
-        ("655_a", "cookbooks"),
-        ("245_a", r"recept[^á\s]"),
-        ("245_a", "kuchařk"),
-        ("245_a", "pečeme"),
-        ("245_a", r"[^\w]pečení"),
-        ("245_a", "vaříme"),
-        ("245_a", "vaření"),
-        ("245_a", "dieta"),
-        ("245_a", "moučník"),
-        ("245_a", "dietn[ěía]"),
-        ("245_a", "obědy"),
-        ("245_a", "večeře"),
-        ("245_a", "těstovin"),
-        ("245_a", "vegetariánsk"),
-        ("245_a", "bezmas"),
-        ("245_a", "vegansk"),
-        ("245_a", "pečivo"),
-        ("245_a", "de[zs]ert[^éoa]"),
-        ("245_a", "pokrm[yů]"),
-        ("245_a", "pomazán[ek]"),
-        ("245_a", "salát"),
-        ("245_a", "gril(ov|ujem)"),
-        ("245_a", " uzení"),
-        ("245_a", "barbecue"),
-        ("245_a", "smažíme"),
-        ("245_a", "polév"),
-        ("245_a", "kuchyně"),
-        ("245_a", "specialit[^a]"),
-        ("245_a", "zavařování"),
-        ("245_a", "nakládání"),
-        ("245_a", "cukroví"),
-        ("490_a", "dobrou chuť"),
-        ("650_a", "moučníky"),
-        ("650_a", "míšené nápoje"),
-        ("650_a", "pečivo"),
-        ("650_a", "grilov"),
-        ("650_a", "uzení"),
-        ("650_a", "zabíjačka"),
-        ("650_a", "konzervování masa"),
-        ("650_a", "nakládání masa"),
-        ("650_a", "sušení potravin"),
-        ("650_a", "konzervování ovoce"),
-        ("650_a", "zmrzliny"),
+        ("650_a", r"(?i)jídla"),
+        ("650_a", r"(?i)meals"),
+        ("650_a", r"(?i)kuchařství"),
+        ("655_a", r"(?i)kuchařské recepty"),
+        ("655_a", r"(?i)cookbooks"),
+        ("245_a", r"(?i)recept[^á\s]"),
+        ("245_a", r"(?i)kuchařk"),
+        ("245_a", r"(?i)kuchařsk"),
+        ("245_a", r"(?i)pečeme"),
+        ("245_a", r"(?i)[^\w]pečení"),
+        ("245_a", r"(?i)vaříme"),
+        ("245_a", r"(?i)vaření"),
+        ("245_a", r"(?i)dieta"),
+        ("245_a", r"(?i)moučník"),
+        ("245_a", r"(?i)dietn[ěía]"),
+        ("245_a", r"(?i)obědy"),
+        ("245_a", r"(?i)večeře"),
+        ("245_a", r"(?i)těstovin"),
+        ("245_a", r"(?i)vegetariánsk"),
+        ("245_a", r"(?i)bezmas"),
+        ("245_a", r"(?i)vegansk"),
+        ("245_a", r"(?i)pečivo"),
+        ("245_a", r"(?i)de[zs]ert[^éoa]"),
+        ("245_a", r"(?i)pokrm[yů]"),
+        ("245_a", r"(?i)pomazán[ek]"),
+        ("245_a", r"(?i)salát"),
+        ("245_a", r"(?i)v .{0,20} kuchyni"),
+        ("245_a", r"(?i)z konzerv"),
+        ("245_a", r"(?i)gril(ov|ujem)"),
+        ("245_a", r"(?i) uzení"),
+        ("245_a", r"(?i)barbecue"),
+        ("245_a", r"(?i)smažíme"),
+        ("245_a", r"(?i)polév"),
+        ("245_a", r"(?i)kuchyně"),
+        ("245_a", r"(?i)specialit[^a]"),
+        ("245_a", r"(?i)zavařování"),
+        ("245_a", r"(?i)nakládání"),
+        ("245_a", r"(?i)cukroví"),
+        ("490_a", r"(?i)dobrou chuť"),
+        ("650_a", r"(?i)moučníky"),
+        ("650_a", r"(?i)míšené nápoje"),
+        ("650_a", r"(?i)pečivo"),
+        ("650_a", r"(?i)grilov"),
+        ("650_a", r"(?i)uzení"),
+        ("650_a", r"(?i)zabíjačka"),
+        ("650_a", r"(?i)kon[zs]ervování masa"),
+        ("650_a", r"(?i)nakládání masa"),
+        ("650_a", r"(?i)sušení potravin"),
+        ("650_a", r"(?i)kon[sz]ervování ovoce"),
+        ("650_a", r"(?i)zmrzliny"),
     ]
 
     filtry_pozitivni = [
@@ -139,7 +146,7 @@ def _(lf, pl):
             pl.concat_list(pl.col("008")).alias("008"),
         )
         .explode(kombo[0], empty_as_null=True)
-        .filter(pl.col(kombo[0]).str.contains(f"(?i){kombo[1]}"))
+        .filter(pl.col(kombo[0]).str.contains(kombo[1]))
         for kombo in sloupec_keyword_pozitivni
     ]
 
@@ -160,9 +167,17 @@ def _(lf, pl):
         ("490_a", "Lidové hry českého jeviště"),
         ("245_a", "Receptář pro fotoamatéry"),
         ("245_a", "100 praktických receptů pro holiče a kadeřníky"),
-        ("245_a", "poslední večeře"),
         ("245_a", "vévodkyně"),
-        ("245_a", "Minecraft"),
+        ("245_a", r"(?i)minecraft"),
+        ("245_a", "Organizace evidence"),
+        ("245_a", "(?i)farmakologi"),
+        ("245_a", "(?i)kon[sz]ervatoř"),
+        ("245_a", "(?i)kon[sz]ervativní"),
+        ("245_a", "pro duši"),
+        ("245_a", r"(?i)poslední večeře"),
+        ("245_a", "rozvozu hotových jídel z centrálních výroben"),
+        ("245_a", "aukce"),
+        ("245_a", "Obsluha v restauracích"),
         ("245_b", "dějství"),
         ("650_a", "odpad"),
         ("650_a", "zbraně"),
@@ -183,7 +198,7 @@ def _(lf, pl):
             pl.concat_list(pl.col("008")).alias("008"),
         )
         .explode(kombo[0], empty_as_null=True)
-        .filter(pl.col(kombo[0]).str.contains(f"(?i){kombo[1]}"))
+        .filter(pl.col(kombo[0]).str.contains(kombo[1]))
         for kombo in sloupec_keyword_negativni
     ]
 
@@ -202,6 +217,7 @@ def _(lf, pl):
 @app.cell
 def _(df, pl):
     df.filter(pl.col("245_a").str.contains("(?i)kuchyně"))
+    return
 
 
 @app.cell
@@ -216,23 +232,29 @@ def _(df, ids_kucharek, pl):
 @app.cell
 def _(df_kucharky, pl):
     df_kucharky.explode("260_b").filter(pl.col("260_b").str.contains("Průmyslové"))
+    return
 
 
 @app.cell
 def _(df_kucharky, pl):
     df_kucharky.explode("260_b").filter(pl.col("260_b").str.contains("spisovatel"))
+    return
 
 
 @app.cell
 def _(df_kucharky, pl):
-    df_kucharky.with_columns(pl.col("245_a").str.to_lowercase().str.split(" ")).explode(
-        "245_a", empty_as_null=True
-    ).group_by("245_a").len().sort(by="len", descending=True)
+    df_kucharky.with_columns(
+        pl.col("245_a").str.to_lowercase().str.split(" ")
+    ).explode("245_a", empty_as_null=True).group_by("245_a").len().sort(
+        by="len", descending=True
+    )
+    return
 
 
 @app.cell
 def _(df_kucharky, pl):
     df_kucharky.filter(pl.col("245_a").str.contains("sociál"))
+    return
 
 
 @app.cell
@@ -245,16 +267,140 @@ def _(df_kucharky, pl):
         "Můžu ochutnat",
         "Kniha plná jídla",
         "(?i)káva",
-        "Python",
-        "Minecraft",
+        # "Python",
+        # "Minecraft",
+        "Jaro v naší kuchyni",
+        "Vaříme pro nemocné chorobami",
+        "Receptury studených pokrmů",
+        "Normy studené kuchyně",
+        "Nová kuchyně",
+        "Zavařování masa v domácnosti",
+        "Domácí konservace",
+        "Zmrazené potraviny",
+        "Rychle a chutně z konzerv",
+        "Zvěřina do naší kuchyně",
+        "Kuchařský lexikon",
+        "Čínská kuchyně",
+        "Počítání nudliček v jarní polévce",
+        "Gastronomický cestopis",
+        "Exotika v družstevní kuchyni",
+        "Houby v kuchyni",
+        "Víkendové jídelníčky",
+        "70 domácích polévek",
+        "70 vaječných jídel",
+        "Vaří šéfkuchař",
+        "Konservování potravin v domácnostech",
+        "Mléčné speciality",
+        "Více sýrů na náš stůl",
+        "Vydatné pokrmy",
+        "Rychlé pokrmy",
+        "Za pět minut dvanáct",
+        "Zelenina v naší kuchyni",
+        "Grilovaná jídla",
+        "Skandinávská kuchyně",
+        "Slavnostní příležitosti",
+        "Zeleninová mísa",
+        "Dneska vařím já",
+        "Kuchyně labužníka",
+        "Ruská kuchyně",
+        "Výživa nejmenších dětí",
+        "Z hotelu Ritz",
+        "Retrojídlo",
+        "Praktické rady pro hospodyně a matky",
+        "Králičí maso v české kuchyni",
+        "Kuře na sto způsobů",
+        "70 bramborových jídel",
     ]
 
+    chybi = []
+
+    print(len(testy))
+
     for t in testy:
-        print(
-            df_kucharky.filter(pl.col("245_a").str.contains(t)).select(
-                pl.col(["100_a", "245_a", "246_a"])
-            )
+        vyfiltr = df_kucharky.filter(pl.col("245_a").str.contains(t)).select(
+            pl.col(["100_a", "245_a", "246_a"])
         )
+        # print("***")
+        # print(t)
+        # print(vyfiltr)
+        if len(vyfiltr) == 0:
+            chybi.append(t)
+
+    chybi
+    return (chybi,)
+
+
+@app.cell
+def _(chybi, df, pl):
+    df.filter(pl.col("245_a").str.contains_any(chybi))
+    return
+
+
+@app.cell
+def _(df_kucharky, pl):
+    nemely_by_byt = [
+        "farmakolog",
+        "Minecraft",
+        "Python",
+        "Kuřecí polévka pro duši",
+        "Slepičí polévka pro duši",
+        "Poslední večeře",
+        "poslední večeře"
+    ]
+
+    df_kucharky.filter(pl.col("245_a").str.contains_any(nemely_by_byt))
+    return
+
+
+@app.cell
+def _(df_kucharky, pl):
+    nalezene_kucharky = df_kucharky.select(pl.col("245_a")).to_series().to_list()
+    return (nalezene_kucharky,)
+
+
+@app.cell
+def _(df, nalezene_kucharky, pl):
+    df.filter(
+        pl.col("245_a").str.contains("(?i)flambo")
+        & (~pl.col("245_a").str.contains_any(nalezene_kucharky))
+    )
+    return
+
+
+@app.cell
+def _(nalezene_kucharky):
+    [x for x in nalezene_kucharky if "Flambo" in x]
+    return
+
+
+@app.cell
+def _(chybi, df, pl):
+    df.filter(pl.col("245_a").str.contains_any(chybi))
+    return
+
+
+@app.cell
+def _(df, pl):
+    df.filter(pl.col("100_7").str.contains("jk01023273"))
+    return
+
+
+@app.cell
+def _(df, pl):
+    df.filter(pl.col("245_a").str.contains(" jídel[^e]")).select(pl.col("245_a"))
+    return
+
+
+@app.cell
+def _(df, pl):
+    df.filter(pl.col("245_a").str.contains("konserv")).select(pl.col("245_a"))
+    return
+
+
+@app.cell
+def _(df, pl):
+    df.explode("260_b").filter(pl.col("260_b").str.contains("Merkur"))
+    return
 
 
 @app.cell
@@ -283,6 +429,7 @@ def _():
     import random
     import re
 
+
     def pocet_stran(rozsah):
         rozsah = str(rozsah)
         cisla = [x for x in re.findall(r"\d{1,4}\s{0,1}[^v]", rozsah)]
@@ -290,6 +437,7 @@ def _():
         if len(cisla) > 0:
             if max(cisla) < 4000:  # jak primitivní, ale jak účinné
                 return max(cisla)
+
 
     def najdi_rok(nulaosm):
         if "u" not in nulaosm:
@@ -302,6 +450,7 @@ def _():
                 return int(nulaosm[7:10] + str(random.randint(0, 9)))
             except:
                 return None
+
 
     print(najdi_rok("000706s1926"))
     print(najdi_rok("000706s192u"))
@@ -332,11 +481,13 @@ def _(df_kucharky, najdi_rok, pl, pocet_stran, vyhodit):
 @app.cell
 def _(df_kucharky_final):
     df_kucharky_final
+    return
 
 
 @app.cell
 def _(df_kucharky_final):
     df_kucharky_final.write_parquet("data/cnb_kucharky.parquet")
+    return
 
 
 @app.cell
@@ -346,6 +497,7 @@ def _(df_kucharky_final, pl):
     ).select(pl.col(["autorstvo", "titul", "rok", "pocet_stran"])).write_csv(
         "data/cnb_kucharky_kontrola.csv"
     )
+    return
 
 
 if __name__ == "__main__":
